89739148b5a5c8d627b24fdd3ab1b077d0268bcb
Kasper Overgaard Mortensen <kasper@post2us.dk>
2024-12-18 11:33:33 +0100

    Adds variants of qbl

--- bandits/bandit_qbl.py
+++ bandits/bandit_qbl.py
@@ -6,24 +6,63 @@ import logging
 
 
 class QBLBandit(Bandit):
-    def __init__(self, arms: list[BanditArm]) -> None:
+    def __init__(self, arms: list[BanditArm], weighted: bool = False) -> None:
         self.arms: list[BanditArm] = arms
-        random.shuffle(self.arms)
+        #random.shuffle(self.arms)
         for i, arm in enumerate(self.arms):
             arm.id = i
-
+        self.arm_names = {}
         self.k: int = len(self.arms)
-
-        self.in_active_term: dict[str, bool] = {arm.index_name: False for arm in self.arms}
-        self.last_term_reward: dict[str, float] = {arm.index_name: 1.0 for arm in self.arms}
-        self.last_term_length: dict[str, int] = {arm.index_name: 1 for arm in self.arms}
-        self.total_last_term_reward: float = float(len(self.arms))
+        
+        self.in_active_term: dict[str, bool] = {}
+        self.last_term_reward: dict[str, float] = {}
+        self.last_term_length: dict[str, int] = {}
+        self.total_last_term_reward: float = 0.0
         self.total_last_term_length: int = len(self.arms)
+        
+        for arm in self.arms:
+            self.in_active_term[arm.index_name] = False
+            self.last_term_reward[arm.index_name] = float(sum(arm.arm_value.values())) if weighted else 1.0
+            self.last_term_length[arm.index_name] = 1
+            self.total_last_term_reward += self.last_term_reward[arm.index_name]
         
         self.queue: PriorityQueue = PriorityQueue(
             [(i, arm) for i, arm in enumerate(self.arms)]
         )
         self.priority: dict[str, int] = {arm.index_name: i for i, arm in enumerate(self.arms)}
+        
+    def expand(self, arms: list[BanditArm], weighted: bool = False) -> None:
+        #Filter list to only include new arms
+        _arms = []
+        for arm in arms:
+            if arm.index_name in self.arm_names:
+                continue
+            _arms.append(arm)
+            self.arm_names[arm.index_name] = True
+        
+            
+        arms = _arms
+        num_new_arms = len(arms)
+        old_k = self.k
+        self.k += num_new_arms
+        self.arms.extend(arms)
+        
+        logging.info(f"Expanding armset with: {num_new_arms}")
+        
+        top: tuple[int, BanditArm] = self.queue.pop()  # type: ignore
+        if top is None:
+            top = (0,0) #No preexisting arms. Dummy top for the loop below.
+        else:
+            self.queue.put(top)  # type: ignore
+        for i, arm in enumerate(arms):
+            arm.id = old_k+i
+            self.in_active_term[arm.index_name] = False
+            self.last_term_reward[arm.index_name] = float(sum(arm.arm_value.values())) if weighted else 1.0
+            self.last_term_length[arm.index_name] = 1
+            self.total_last_term_reward += float(sum(arm.arm_value.values())) if weighted else 1.0
+            self.total_last_term_length += 1
+            self.priority[arm.index_name] = top[0] - arm.id - 1
+            self.queue.put((self.priority[arm.index_name], arm))
 
     def select_arms(self, m: int, current_round: int) -> list[int]:
         selected: list[int] = []
@@ -32,6 +71,29 @@ class QBLBandit(Bandit):
             pop: tuple[int, BanditArm] = self.queue.pop()  # type: ignore
             popped.append(pop)
             selected.append(pop[1].id)
+
+        i: tuple[int, BanditArm]
+        for i in popped:
+            self.queue.put(i)  # type: ignore
+
+        return selected
+    
+    def select_arms_budget(self, budget: int,) -> list[int]:
+        selected: list[int] = []
+        popped: list[tuple[int, BanditArm]] = []
+        used_budget = 0
+        while used_budget < budget*0.95:
+            pop: tuple[int, BanditArm] = self.queue.pop()  # type: ignore
+            if pop is None: #No arms left to choose
+                logging.warning(f"Checked all arms to fill memory budget. Used {used_budget} of {budget}")
+                break
+            if used_budget + pop[1].memory < budget:
+                used_budget += pop[1].memory
+                selected.append(pop[1].id)
+            elif pop[1].memory > budget:
+                logging.warning(f"Arm {pop[1].id} is too big to ever select. {pop[1].memory}. Removed from queue")
+                continue
+            popped.append(pop)
 
         i: tuple[int, BanditArm]
         for i in popped:
